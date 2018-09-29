0e6bf28581fa57f5a920792e104073d5f5a061a1
John Williams <Johnwilliams199032@gmail.com>
2018-09-29 18:48:05 -0400

    restructuring linked list for adaptability to circular linked list, and making main have controm of the free()

--- linked_list.c
+++ linked_list.c
@@ -11,6 +11,10 @@
 		// generate queue at onset of schedule specific function
 		// via function and destroy as function goes??
 		// mean generating an array to hold items--> dynamic array.....
+//fixed:
+// made the linked list pop_back return item. 
+// try to push item back into queue. --> make sure that we can do it curcularly and make sure push takes in a PCB correctly
+
 
 typedef struct pcb_t {
 int compTime;
@@ -70,30 +74,45 @@ if ( curPtr == NULL)
 void pop( nodePtr * arg, int index)
 {
 
-if ((*arg) == NULL)
-{
-printf("HERE\n\n");
-return;
-}
-
-
-int counter = 0;
-struct node* curPtr = (*arg);
-struct node* prev = (*arg)->prev;
-
+  if ((*arg) == NULL)
+  {
+  printf("HERE\n\n");
+  return;
+  }
+  
+  
+  int counter = 0;
+  struct node* curPtr = (*arg);
+  struct node* prev = (*arg)->prev;
+  //struct node* tmp = curPtr;
+  
   while (curPtr->next != NULL && counter < index)
   {
     prev = curPtr;
     curPtr = curPtr->next;
     counter +=1;
   }
+  if(prev != NULL){
   prev->next = curPtr->next;
+  }
   curPtr->next->prev = prev;
+  curPtr->prev->next = curPtr->next;
+
+
+  free(curPtr->process);
   free(curPtr);
+
 }
 
 // pop element off of end
-void pop_back( nodePtr * arg)
+
+void freeNode(nodePtr * arg) {
+  free((*arg)->process);
+  free((*arg));
+  return;
+}
+
+ nodePtr pop_back( nodePtr * arg)
 {
   int counter = 0;
   struct node* curPtr = (*arg);
@@ -102,11 +121,10 @@ void pop_back( nodePtr * arg)
   if (curPtr == NULL) { exit(0);  }
   if (curPtr->next == NULL && curPtr->prev == NULL)
   {
-      
-    free((*arg)->process);
-    free((*arg));
-    (*arg) = NULL;
-    return;
+    return curPtr;
+   // free((*arg)->process);
+   // free((*arg));
+   // (*arg) = NULL;
   }
 
   while (curPtr->next != NULL)
@@ -116,13 +134,14 @@ void pop_back( nodePtr * arg)
     counter +=1;
   }
   if (prev != NULL) {
-	  prev->next = curPtr->next;
+    prev->next = curPtr->next;
   }
 
-  free(curPtr->process);
-  curPtr->process = NULL;
-  free(curPtr);
-  curPtr = NULL;
+  return curPtr;
+  //free(curPtr->process);
+  //curPtr->process = NULL;
+  //free(curPtr);
+  //curPtr = NULL;
 }
 
 void print( nodePtr * arg)
@@ -133,19 +152,19 @@ void print( nodePtr * arg)
   {
     return; 
   }
-   printf("compTime    %d\n ",curPtr->process->compTime    );
-   printf("waitTime    %d\n ",curPtr->process->waitTime    );
-   printf("turnTime    %d\n ",curPtr->process->turnTime    );
-   printf("respTime    %d\n ",curPtr->process->respTime    );
-   printf("contextCount%d\n\n ",curPtr->process->contextCount);
+   printf("compTime       %d\n ",curPtr->process->compTime    );
+   printf("waitTime       %d\n ",curPtr->process->waitTime    );
+   printf("turnTime       %d\n ",curPtr->process->turnTime    );
+   printf("respTime       %d\n ",curPtr->process->respTime    );
+   printf("contextCount   %d\n\n ",curPtr->process->contextCount);
   while (curPtr->next != NULL)
   {
    //printf("%d\n ",curPtr->data);
-   printf("compTime    %d\n ",curPtr->next->process->compTime    );
-   printf("waitTime    %d\n ",curPtr->next->process->waitTime    );
-   printf("turnTime    %d\n ",curPtr->next->process->turnTime    );
-   printf("respTime    %d\n ",curPtr->next->process->respTime    );
-   printf("contextCount%d\n\n ",curPtr->next->process->contextCount);
+   printf("compTime      %d\n ",curPtr->next->process->compTime    );
+   printf("waitTime      %d\n ",curPtr->next->process->waitTime    );
+   printf("turnTime      %d\n ",curPtr->next->process->turnTime    );
+   printf("respTime      %d\n ",curPtr->next->process->respTime    );
+   printf("contextCount  %d\n\n ",curPtr->next->process->contextCount);
    curPtr = curPtr->next;
   }
    printf("\n\n");
@@ -160,6 +179,7 @@ nodePtr head = NULL;
 //newNode(&head);
 nodePtr currentPCB = calloc(1, sizeof(struct node));
 currentPCB->process = calloc(1, sizeof(struct pcb_t));
+
 //  printf("outside:%d\n", head->data);
 for(int i = 0; i < 10 ; i ++)
 {
@@ -172,7 +192,16 @@ currentPCB->process->compTime = i;
 
 for(int i = 0; i < 10 ; i ++)
 {
-  pop_back(&head);
+currentPCB = pop_back(&head);
+
+   printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n ");
+   printf("compTime       %d\n "  ,currentPCB->process->compTime    );
+   printf("waitTime       %d\n "  ,currentPCB->process->waitTime    );
+   printf("turnTime       %d\n "  ,currentPCB->process->turnTime    );
+   printf("respTime       %d\n "  ,currentPCB->process->respTime    );
+   printf("contextCount   %d\n\n ",currentPCB->process->contextCount);
+   printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n ");
+
   printf("pop iteration %d\n",i);
   print(&head);
 }
