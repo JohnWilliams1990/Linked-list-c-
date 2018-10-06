dd23080319560c65e141db9c7a4f0a3f91fcf9e5
John Williams <Johnwilliams199032@gmail.com>
2018-10-06 02:14:43 -0600

    Delete gat.sh

--- gat.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#  John Williams
-#  105201054
-#  Date: Sat. Oct. 6, 2018
-#  Program: gat.sh
-git add * ; git commit -m '$@' ; git pull origin pcb ; git push origin pcb
-
-echo "git add * ; git commit -m '$@' ; git pull origin pcb ; git push origin pcb"
