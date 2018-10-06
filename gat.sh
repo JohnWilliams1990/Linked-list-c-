50acd0efe0ee2ec33598569fccef01b6cf72b413
John Williams <Johnwilliams199032@gmail.com>
2018-10-06 03:31:48 -0400

    $@

--- gat.sh
+++ gat.sh
@@ -3,9 +3,4 @@
 #  105201054
 #  Date: Sat. Oct. 6, 2018
 #  Program: gat.sh
-
-
-echo "git add * ; git commit -m '$@' ; git pull origin pcb ; git push origin pcb"
 git add * ; git commit -m '$@' ; git pull origin pcb ; git push origin pcb
-
-
